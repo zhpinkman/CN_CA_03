5de615f098a6bf6fcef4d2b3745904ac075b933a
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-07-13 13:45:05 +0430

    Change in main

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import multiprocessing
 import threading
 import time
 
+from P2PNode import P2PNode
+
 UDP_IP = "localhost"
 UDP_PORTs = [9001, 9002, 9003, 9004, 9005, 9006]
 
 
-
-
-def p2p_task(port):
-    p2p_node = P2PNode(port)
+def p2p_task(udp_ip, port):
+    p2p_node = P2PNode(udp_ip, port)
 
 
 def main():
     print("Hi")
     processes_list = []
     for port in UDP_PORTs:
-        process = multiprocessing.Process(target=p2p_task, args=(port,))
+        process = multiprocessing.Process(target=p2p_task, args=(UDP_IP, port,))
         process.run()
         process.start()
         processes_list.append(process)
