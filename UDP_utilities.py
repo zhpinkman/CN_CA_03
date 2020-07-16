31c37059a991fc9875d5501ff4be6b010a077024
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-07-16 21:41:37 +0430

    Close socket in send to

--- UDP_utilities.py
+++ UDP_utilities.py
@@ -9,3 +9,4 @@ import pickle
 def send_to(obj, udp_ip, udp_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(pickle.dumps(obj), (udp_ip, udp_port))
+    sock.close()
